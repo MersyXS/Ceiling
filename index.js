273d82a9cf69bfcca4e14c8b8fb2f770d546ab00
Egor Goncharov <egor.goncharov99@mail.ru>
2022-10-29 06:23:23 +0700

    Обновил бургер

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+let page = document.querySelector('.page');
 let popup = document.querySelector('.popup');
 let buttonOpen = document.querySelector('.header__burger');
 let buttonClose = document.querySelector('.popup__close-button');
+let cbox = document.querySelectorAll(".popup__url");
+
+for (let i = 0; i < cbox.length; i++) {
+    cbox[i].addEventListener("click", closePopup);
+    };
+
 
 function openPopup(){
-    popup.classList.add('popup_opened')
-    console.log("123");
+    popup.classList.add('popup_opened');
+    page.classList.add('page_overflow');
 }
 
 function closePopup(){
-    popup.classList.remove('popup_opened')
-    console.log("321");
+        popup.classList.remove('popup_opened');
+        page.classList.remove('page_overflow');
 }
+
 
 buttonOpen.addEventListener('click', openPopup);
 buttonClose.addEventListener('click', closePopup);
