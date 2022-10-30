e2fd83550ce8ecedb20c211ea7166d70cb10b5bf
Egor Goncharov <egor.goncharov99@mail.ru>
2022-10-30 17:04:58 +0700

    Закончил адаптив

--- index.js
+++ index.js
@@ -20,5 +20,20 @@ function closePopup(){
 }
 
 
+document.querySelectorAll('a[href^="#"').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        let href = this.getAttribute('href').substring(1);
+        const scrollTarget = document.getElementById(href);
+        const topOffset = document.querySelector('.scroll').offsetHeight;
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset;
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
+
 buttonOpen.addEventListener('click', openPopup);
 buttonClose.addEventListener('click', closePopup);
