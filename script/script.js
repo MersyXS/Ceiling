a3f73f42c5debbd912f46c238a1b81b61fefb1e8
Egor Goncharov <egor.goncharov99@mail.ru>
2023-01-24 17:32:38 +0700

    Доделываю логигу калькулятора

--- script/script.js
+++ script/script.js
@@ -40,9 +40,105 @@ document.querySelectorAll('a[href^="#"').forEach((link) => {
 buttonOpen.addEventListener("click", openPopup);
 buttonClose.addEventListener("click", closePopup);
 
-function rangeClick() {
-  const rangeInput = document.querySelector(".form__range");
-  const rangeCount = document.querySelector(".form__count");
+const rangeInput = document.querySelector(".form__range");
+const rangeCount = document.querySelector(".form__count");
+var summ = 0;
+const textSumm = document.querySelector('.calculator__summa');
+const type = document.querySelector('#form__type')
 
-  rangeCount.textContent = rangeInput.value + ` м.`;
-}
+//Калькульятор, количество светильников
+const svetPlus = document.querySelector('.control__svet-plus');
+const svetMinus  = document.querySelector('.control__svet-minus');
+const svetImput = document.querySelector('.input-svet');
+var svetCount = 0;
+
+svetPlus.addEventListener('click', function addCount(){
+svetCount = svetCount + 1;
+svetImput.value = svetCount;
+summ = summ + parseInt(type.value);
+textSumm.textContent = summ;
+})
+
+svetMinus.addEventListener('click', function addCount(){
+  svetCount = svetCount - 1;
+  svetImput.value = svetCount;
+  summ = summ - parseInt(type.value);
+  textSumm.textContent = summ;
+})
+
+//Калькульятор, количество люстр
+const chandelierPlus = document.querySelector('.control__chandelier-plus');
+const chandelierMinus  = document.querySelector('.control__chandelier-minus');
+const chandelierImput = document.querySelector('.input__chandelier');
+var chandelierCount = 0;
+
+
+chandelierPlus.addEventListener('click', function addCountSvet(){
+  chandelierCount = chandelierCount + 1;
+  chandelierImput.value = chandelierCount;
+  summ = summ + parseInt(type.value);
+  textSumm.textContent = summ;
+  })
+  
+chandelierMinus.addEventListener('click', function addCountChandelier(){
+    chandelierCount = chandelierCount - 1;
+    chandelierImput.value = chandelierCount;
+    summ = summ - parseInt(type.value);
+    textSumm.textContent = summ;
+  })
+
+
+//Калькульятор, количество углов
+const seePlus = document.querySelector('.control__see-plus');
+const seeMinus  = document.querySelector('.control__see-minus');
+const seeImput = document.querySelector('.input-see');
+var seeCount = 0;
+
+
+seePlus.addEventListener('click', function addCountSee(){
+  seeCount = seeCount + 1;
+  seeImput.value = seeCount;
+  summ = summ + parseInt(type.value);
+  textSumm.textContent = summ;
+  })
+  
+  seeMinus.addEventListener('click', function addCountSee(){
+  seeCount = seeCount - 1;
+  seeImput.value = seeCount;
+  summ = summ - parseInt(type.value);
+  textSumm.textContent = summ;
+  })
+
+
+//Калькульятор, количество труб
+  const pipesPlus = document.querySelector('.control__pipes-plus');
+  const pipesMinus  = document.querySelector('.control__pipes-minus');
+  const pipesImput = document.querySelector('.input__pipes');
+  var pipesCount = 0;
+
+
+pipesPlus.addEventListener('click', function addCountPipes(){
+  pipesCount = pipesCount + 1;
+  pipesImput.value = pipesCount;
+  summ = summ + 1999;
+  textSumm.textContent = summ;
+
+  var summPipes = summ;
+  console.log(summPipes)
+  return summPipes;
+ 
+})
+  
+  pipesMinus.addEventListener('click', function addCountPipes(){
+    pipesCount = pipesCount - 1;
+    pipesImput.value = pipesCount;
+    summ = summ - 1999;
+  })
+
+
+  function rangeClick(summPipes) {
+    rangeCount.textContent = rangeInput.value;
+    summ = parseInt(rangeCount.textContent) * type.value;
+    textSumm.textContent = summ + summPipes;
+    console.log(summPipes)
+  }
